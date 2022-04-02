e2077561dc36be23f13512993dcc01a87970b397
A.SMAHI <abderrahim.smahi99@gmail.com>
2022-04-02 10:13:04 -0500

    created hello world 100 time

--- src/day20/TestCase003.java
+++ src/day20/TestCase003.java
@@ -3,7 +3,14 @@ package day20;
 public class TestCase003 {
     public static void main(String[] args) {
         System.out.println("Hello World");
+
+
+        for(int i = 0; i <= 100; i++){
+            System.out.println("Hello World");
+        }
     }
 
     // Made some changement in the code ...
+
+
 }
